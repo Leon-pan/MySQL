64e006c05e4515d88669ad6f5040a1dff310705e
Administrator <pjl@hleast.com>
2020-06-08 13:46:25 +0800

    time update

--- backup_dbfile.sh
+++ backup_dbfile.sh
@@ -5,5 +5,5 @@ mkdir -p /home/mysql_data/mysql_backup/dbfiles/${strdate}
 cp -rf `ls |grep -v mysql-bin|grep -v sock` -t "/home/mysql_data/mysql_backup/dbfiles/${strdate}/"   #复制全数据库文件
 tar -czf ${strdate}.tar.gz ${strdate}  #打包压缩
 rm -rf /home/mysql_data/mysql_backup/dbfiles/${strdate}  #删除原始文件
-find /home/mysql_data/mysql_backup/dbfiles/ -mtime +30    -name "201*"  -exec rm -rf {} \;   #删除30天前的备份
+find /home/mysql_data/mysql_backup/dbfiles/ -mtime +30    -name "202*"  -exec rm -rf {} \;   #删除30天前的备份
 exit 0
